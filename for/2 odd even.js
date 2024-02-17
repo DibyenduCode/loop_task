2d1f8229eb861b02c30689d75c24aeffd7e71f38
DibyenduCode <khaskeldibyendu22@gmail.com>
2024-02-17 21:40:22 +0530

    3rd

--- for/2 odd even.js
+++ for/2 odd even.js
@@ -15,4 +15,21 @@ Find all the even numbers from 78 to 98.
 
  */
 
-/*programming hero*/
+/*Dibyendy code*/
+
+//Ans:
+
+//Find all the odd numbers from 61 to 100.
+
+// for(let i=78; i<=; i++){
+//     if(i%2==0){
+//         console.log(i);
+//     }
+// }
+
+for(let i=61; i<=100; i++){
+        if(i%2!==0){
+            console.log(i);
+        }
+    }
+
