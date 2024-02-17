d1010b84bf95bcd5bee03e18fe933b29b55b136e
DibyenduCode <khaskeldibyendu22@gmail.com>
2024-02-17 22:13:54 +0530

    4th

--- for/3 sum of odd numbers.js
+++ for/3 sum of odd numbers.js
@@ -15,4 +15,30 @@ Display sum of all the even numbers from 51 to 85.
 
  */
 
-/*programming hero*/
+/*Dibyendu code*/
+
+// Ans
+
+//Display sum of all the odd numbers from 91 to 129.
+
+
+// let sum=0;
+// for(let i=91; i<=129; i++){
+//             if(i %2 !== 0){
+//                 sum=sum+i; 
+//             }
+//         }   
+// console.log(sum);
+
+
+
+//Display sum of all the even numbers from 51 to 85.
+
+let sum=0;
+for(let i=51; i<=85; i++){
+            if(i %2 == 0){
+                sum=sum+i; 
+            }
+        }   
+console.log(sum);
+    
