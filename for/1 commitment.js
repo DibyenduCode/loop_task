03b0f7dc9b469a037a73fda0197dbd6a1ab44bd8
DibyenduCode <khaskeldibyendu22@gmail.com>
2024-02-17 21:28:32 +0530

    2nd

--- for/1 commitment.js
+++ for/1 commitment.js
@@ -4,4 +4,11 @@
 
  */
 
-/*programming hero*/
+/*Dibyendu Code*/
+
+let text="I will invest at least 6 hrs every single day for next 60 days!";
+
+for(let i=1; i<=60;i++){
+    console.log(text,i);
+}
+
